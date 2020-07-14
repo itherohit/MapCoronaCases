17f7e7e5e5c969a5c602193b989f207823cea492
Rohit <rohitshan751@gmail.com>
2020-07-14 13:34:34 +0530

    static files added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require("path");
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
