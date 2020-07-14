7b1a7b176d44f80232626aa786e8f1637ab84882
Rohit <rohitshan751@gmail.com>
2020-07-14 13:18:31 +0530

    gitignore

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ const commaNumber = require('comma-number');
 const path = require("path");
 const app = express();
 
-process.env.PWD = process.cwd();
-app.use(express.static(process.env.PWD + '/public'));
-console.log(process.env.PWD + '/public');
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
