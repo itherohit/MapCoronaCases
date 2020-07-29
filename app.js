35463695f2be803a78da34dacc515949616ddbef
Rohit <rohitshan751@gmail.com>
2020-07-29 15:29:31 +0530

    india api changed

--- app.js
+++ app.js
@@ -125,79 +125,88 @@ app.get("/india", IndiaLimiter, function(req, resp) {
         'type': 'FeatureCollection',
         'features': []
     };
-    request('https://api.covidindiatracker.com/total.json', { json: true }, (err, res, body) => {
-        if (err) {
-            resp.render("error");
+    var options = {
+        method: 'GET',
+        url: 'https://corona-virus-world-and-india-data.p.rapidapi.com/api_india',
+        headers: {
+            'x-rapidapi-host': 'corona-virus-world-and-india-data.p.rapidapi.com',
+            'x-rapidapi-key': '0f7069a4fdmshdae9a27babc8b78p1e15fdjsn657382ee1e75',
+            useQueryString: true
         }
-        body.country = "India";
-        body.fcases = fnum(body.confirmed);
-        body.frecovered = fnum(body.recovered);
-        body.fdeaths = fnum(body.deaths);
-        body.cases = body.confirmed;
-        body.recovered = body.recovered;
-        body.deaths = body.deaths;
-        body.img = "https://mapcoronacases.s3-ap-northeast-1.amazonaws.com/indianflag.png";
-        body.latlng = [79.029432, 22.364293];
-        var geo = {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': body.latlng
-            },
-            'properties': {
-                'description': "<div class='popupheader'><h3>" + body.country + "</h3></div><div class='popupdata'><p>Total Cases</p><p>" + commaNumber(body.cases) + "</p></div><div class='popupdata'><p>Recovered</p><p>" + commaNumber(body.recovered) + "</p></div><div class='popupdata'><p>Deaths </p><p>" + commaNumber(body.deaths) + "</p></div>"
+    };
+    request(options, function(error, response, body) {
+        if (error) {
+            resp.render("error");
+        } else {
+            const data = {};
+            body = JSON.parse(body);
+            data.country = "India";
+            data.fcases = fnum(body.total_values.confirmed);
+            data.frecovered = fnum(body.total_values.recovered);
+            data.fdeaths = fnum(body.total_values.deaths);
+            data.active = body.total_values.active;
+            data.cases = body.total_values.confirmed;
+            data.recovered = body.total_values.recovered;
+            data.deaths = body.total_values.deaths;
+            data.img = "https://mapcoronacases.s3-ap-northeast-1.amazonaws.com/indianflag.png";
+            data.latlng = [79.029432, 22.364293];
+            var geo = {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': data.latlng
+                },
+                'properties': {
+                    'description': "<div class='popupheader'><h3>" + data.country + "</h3></div><div class='popupdata'><p>Active Cases</p><p>" + commaNumber(data.active) + "</p></div><div class='popupdata'><p>Total Cases</p><p>" + commaNumber(data.cases) + "</p></div><div class='popupdata'><p>Recovered</p><p>" + commaNumber(data.recovered) + "</p></div><div class='popupdata'><p>Deaths </p><p>" + commaNumber(data.deaths) + "</p></div>"
+                }
             }
-        }
-        geo_json.features.push(geo);
-        data_json.push(body);
-    });
-    request('https://api.covidindiatracker.com/state_data.json', { json: true }, (err, res, body) => {
-        if (err) {
-            resp.render("error");
-        }
-        body.forEach(data => {
-            if (data.state != "Unknown*") {
-                data.country = data.state;
-                data.fcases = fnum(data.confirmed);
-                data.frecovered = fnum(data.recovered);
-                data.fdeaths = fnum(data.deaths);
-                data.cases = data.confirmed;
-                data.recovered = data.recovered;
-                data.deaths = data.deaths;
-                data.img = "https://mapcoronacases.s3-ap-northeast-1.amazonaws.com/markermap.png";
+            geo_json.features.push(geo);
+            data_json.push(data);
+
+            for (var key in body.state_wise) {
+                const statedata = {};
+                statedata.country = key;
+                statedata.active = body.state_wise[key].active;
+                statedata.fcases = fnum(body.state_wise[key].confirmed);
+                statedata.frecovered = fnum(body.state_wise[key].recovered);
+                statedata.fdeaths = fnum(body.state_wise[key].deaths);
+                statedata.cases = body.state_wise[key].confirmed;
+                statedata.recovered = body.state_wise[key].recovered;
+                statedata.deaths = body.state_wise[key].deaths;
+                statedata.img = "https://mapcoronacases.s3-ap-northeast-1.amazonaws.com/markermap.png";
                 for (var i = 0; i < india.length; i++) {
-                    if (india[i].name == data.country) {
-                        data.latlng = [india[i].latlng[1], india[i].latlng[0]];
+                    if (india[i].name == statedata.country) {
+                        statedata.latlng = [india[i].latlng[1], india[i].latlng[0]];
                         var geo = {
                             'type': 'Feature',
                             'geometry': {
                                 'type': 'Point',
-                                'coordinates': data.latlng
+                                'coordinates': statedata.latlng
                             },
                             'properties': {
-                                'description': "<div class='popupheader'><h3>" + data.country + "</h3></div><div class='popupdata'><p>Active Cases</p><p>" + commaNumber(data.active) + "</p></div></div><div class='popupdata'><p>Total Cases</p><p>" + commaNumber(data.cases) + "</p></div><div class='popupdata'><p>Recovered</p><p>" + commaNumber(data.recovered) + "</p></div><div class='popupdata'><p>Deaths </p><p>" + commaNumber(data.deaths) + "</p></div><div class='more-info'><a href=/india/" + data.id + ">District Data</a></div>"
+                                'description': "<div class='popupheader'><h3>" + statedata.country + "</h3></div><div class='popupdata'><p>Active Cases</p><p>" + commaNumber(statedata.active) + "</p></div></div><div class='popupdata'><p>Total Cases</p><p>" + commaNumber(statedata.cases) + "</p></div><div class='popupdata'><p>Recovered</p><p>" + commaNumber(statedata.recovered) + "</p></div><div class='popupdata'><p>Deaths </p><p>" + commaNumber(statedata.deaths) + "</p></div><div class='more-info'><a href=/india/" + body.state_wise[key].statecode + ">District Data</a></div>"
                             }
                         }
                     }
-                    if (!data.latlng) {
-                        data.latlng = [0, 0];
+                    if (!statedata.latlng) {
+                        statedata.latlng = [0, 0];
                         var geo = {
                             'type': 'Feature',
                             'geometry': {
                                 'type': 'Point',
-                                'coordinates': data.latlng
+                                'coordinates': statedata.latlng
                             },
                             'properties': {
-                                'description': "<div class='popupheader'><h3>" + data.country + "</h3></div><div class='popupdata'><p>Active Cases</p><p>" + commaNumber(data.active) + "</p></div></div><div class='popupdata'><p>Total Cases</p><p>" + commaNumber(data.cases) + "</p></div><div class='popupdata'><p>Recovered</p><p>" + commaNumber(data.recovered) + "</p></div><div class='popupdata'><p>Deaths </p><p>" + commaNumber(data.deaths) + "</p></div><div class='more-info'><a href=/india/" + data.id + " >District Data</a></div>"
+                                'description': "<div class='popupheader'><h3>" + statedata.country + "</h3></div><div class='popupdata'><p>Active Cases</p><p>" + commaNumber(statedata.active) + "</p></div></div><div class='popupdata'><p>Total Cases</p><p>" + commaNumber(statedata.cases) + "</p></div><div class='popupdata'><p>Recovered</p><p>" + commaNumber(statedata.recovered) + "</p></div><div class='popupdata'><p>Deaths </p><p>" + commaNumber(statedata.deaths) + "</p></div><div class='more-info'><a href=/india/" + body.state_wise[key].statecode + ">District Data</a></div>"
                             }
                         }
                     }
                 }
                 geo_json.features.push(geo);
-                data_json.push(data);
+                data_json.push(statedata);
             }
-        });
-        resp.render("index", { data_json: data_json, geo_json: JSON.stringify(geo_json) });
+            resp.render("index", { data_json: data_json, geo_json: JSON.stringify(geo_json) });
+        }
     });
 })
 
@@ -218,17 +227,28 @@ app.get("/about", function(req, res) {
 })
 
 app.get("/india/:id", function(req, resp) {
-    var state = req.params.id;
-    console.log(state);
-    request('https://api.covidindiatracker.com/state_data.json', { json: true }, (err, res, body) => {
-        if (err) {
-            resp.render("error");
+    const state = req.params.id;
+    var options = {
+        method: 'GET',
+        url: 'https://corona-virus-world-and-india-data.p.rapidapi.com/api_india',
+        headers: {
+            'x-rapidapi-host': 'corona-virus-world-and-india-data.p.rapidapi.com',
+            'x-rapidapi-key': '0f7069a4fdmshdae9a27babc8b78p1e15fdjsn657382ee1e75',
+            useQueryString: true
         }
-        body.forEach(data => {
-            if (_.toLower(state) === _.toLower(data.id)) {
-                resp.render("districts", { district_json: data.districtData });
+    };
+
+    request(options, function(error, response, body) {
+        if (error) {
+            resp.render("error");
+        } else {
+            body = JSON.parse(body);
+            for (var keys in body.state_wise) {
+                if (_.toLower(state) === _.toLower(body.state_wise[keys].statecode)) {
+                    resp.render("districts", { district_json: body.state_wise[keys].district });
+                }
             }
-        });
+        }
     });
 })
 
